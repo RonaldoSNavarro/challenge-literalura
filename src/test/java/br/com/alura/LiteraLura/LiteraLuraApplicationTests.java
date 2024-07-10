b28251a81f7b020e3941d60fcd5d316eacfbb2f7
ronaldosnavarro <navarro.silva@terra.com.br>
2024-07-09 22:23:17 -0300

    Initial commit

---
+++ src/test/java/br/com/alura/LiteraLura/LiteraLuraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.LiteraLura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraLuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
